53e755cb964bd2e53fe02154c596e301947ce0bc
jhndagon <jhndagon@gmail.com>
2020-02-08 04:57:07 -0500

    Query parameter 2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>Prueba Heroku Laura</title>
  </head>
  <body>
- <?php echo '¡Hola' . htmlspecialchars($_GET["nombre"]) . '!'; ?>
+ <?php echo '¡Hola ' . htmlspecialchars($_GET["name"]) . '!'; ?>
  </body>
 </html>
 
